0db5e6d45c709e9ac96352f272656fe4604010d2
oguni <oguni@b-forme.net>
2020-04-28 14:30:03 +0900

    シーディング修正（テスト環境ではカナ名表記に変更）

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,11 +7,15 @@ use Faker\Generator as Faker;
 
 
 $factory->define(User::class, function (Faker $faker) {
+
+    $last_name = $faker->lastKanaName;
+    $first_name = $faker->firstKanaName;
+
     return [
-        'last_name' => $faker->lastName,
-        'first_name' => $faker->firstName,
-        'last_name_kana' =>  $faker->firstKanaName,
-        'first_name_kana' => $faker->lastKanaName,
+        'last_name' => $last_name,
+        'first_name' => $first_name,
+        'last_name_kana' => $last_name,
+        'first_name_kana' => $first_name,
         'school_id' => $faker->numberBetween(1, 2),
     ];
 });
