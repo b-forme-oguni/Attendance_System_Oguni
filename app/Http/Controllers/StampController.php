cbdd99199778d478ea0f20ade0698834a2a672b2
oguni <oguni@b-forme.net>
2020-05-14 16:45:01 +0900

    【打刻画面】出席・退席の状態表示

--- app/Http/Controllers/StampController.php
+++ app/Http/Controllers/StampController.php
@@ -11,8 +11,8 @@ use Illuminate\Support\Carbon;
 class StampController extends Controller
 {
 
+    // 五十音を解列で格納
     const  KANA = array(
-        // 五十音を解列で格納
         'ア' => ['ア', 'イ', 'ウ', 'エ', 'オ'],
         'カ' => ['カ', 'ガ', 'キ', 'ギ', 'ク', 'グ', 'ケ', 'ゲ', 'コ', 'ゴ'],
         'サ' => ['サ', 'ザ', 'シ', 'ジ', 'ス', 'ズ', 'セ', 'ゼ', 'ソ', 'ゾ'],
@@ -25,6 +25,7 @@ class StampController extends Controller
         'ワ' => ['ワ', 'ヲ', 'ン'],
     );
 
+    //打刻画面表示
     public function stamp(Request $request, $school_id)
     {
         $school =  School::idEqual($school_id)->first();
@@ -34,50 +35,78 @@ class StampController extends Controller
         $dt = new \Carbon\Carbon();
         $today = $dt->isoFormat('YYYY年MM月DD日（ddd）');
 
+        // 利用者リストを学校idでスコープ
+        $userslist = User::schoolIdEqual($school_id)->get();
+
+        //利用者の出席状態を連想配列で記録
+        $attendlist = array();
+        foreach ($userslist as $user) {
+            $timestamp = Performance::where('user_id', $user->id)->latest()->first();
+            if ($timestamp) {
+                $oldTimestampDay = $timestamp->insert_date;
+                $newTimestampDay = Carbon::now()->toDateString();
+                if (($oldTimestampDay == $newTimestampDay)) {
+                    if (!$timestamp->end) {
+                        $attendlist[$user->id] = true;
+                    } else {
+                        $attendlist[$user->id] = false;
+                    }
+                }
+            }
+        }
+
+        // 利用者リストを学校idでスコープ
+        $userstable = User::schoolIdEqual($school_id);
+
         // 利用者リストを目次で絞る
         $index = $request->index;
         if ($index) {
             // 指定行のユーザーのみ表示
             $kanaindex = self::KANA[$index];
-            $param = '';
+            $initial = '';
             for ($i = 0; $i < count($kanaindex); $i++) {
                 if ($i == count($kanaindex) - 1) {
-                    $param .= '"' . $kanaindex[$i] . '"';
+                    $initial .= '"' . $kanaindex[$i] . '"';
                 } else {
-                    $param .= '"' . $kanaindex[$i] . '", ';
+                    $initial .= '"' . $kanaindex[$i] . '", ';
                 }
             }
-            $sqltxt = 'left(last_name_kana, 1) in (' . $param . ')';
-            $users = User::schoolIdEqual($school_id)
+            $sqltxt = 'left(last_name_kana, 1) in (' . $initial . ')';
+            $users = $userstable
                 ->whereRaw($sqltxt)
                 ->orderBy('last_name_kana')
                 ->get();
         } else {
             // 全てのユーザーを表示
-            $users =  User::schoolIdEqual($school_id)
-                ->orderBy('last_name_kana')->get();
-        }
-
-        // 利用者名を表示
-        if ($request->id) {
-            $personal = User::where('id', $request->id)->first();
-        } else {
-            $personal = '';
+            $users = $userstable
+                ->orderBy('last_name_kana')
+                ->get();
         }
 
         $prame = [
             'school' => $school,
             'today' => $today,
             'users' => $users,
-            'personal' => $personal,
             'kanalist' => self::KANA,
+            'attendlist' =>  $attendlist,
         ];
+
+        // 利用者名を表示
+        if ($request->id) {
+            $personal = User::where('id', $request->id)->first();
+            $prame['personal'] = $personal;
+        } else {
+            $alerttxt = '右リストから利用者名を選択して下さい';
+            $prame['alerttxt'] = $alerttxt;
+        }
+
         return view('user.stamp', $prame);
     }
 
+    //開始打刻
     public function start(Request $request, $school_id)
     {
-
+        //利用者の前回のレコードがあれば、最終打刻の日付を取得
         $oldTimestamp = Performance::where('user_id', $request->user_id)->latest()->first();
         if ($oldTimestamp) {
             $oldTimestampDay = $oldTimestamp->insert_date;
@@ -85,11 +114,13 @@ class StampController extends Controller
             $oldTimestampDay = '';
         }
 
+        //当日2回目の打刻は行わない
         $newTimestampDay = Carbon::now()->toDateString();
         if (($oldTimestampDay == $newTimestampDay)) {
             return  redirect('stamp/' . $school_id);
         }
 
+        //当日のレコードを作成
         $timestamp = Performance::create([
             'user_id' => $request->user_id,
             'insert_date' => $newTimestampDay,
@@ -99,18 +130,14 @@ class StampController extends Controller
         return  redirect('stamp/' . $school_id);
     }
 
-
+    //終了打刻
     public function end(Request $request, $school_id)
     {
-
+        //終了打刻がまだであれば、レコードに終了時刻を追加変更
         $timestamp = Performance::where('user_id', $request->user_id)->latest()->first();
-
-
         if (!empty($timestamp->end)) {
             return  redirect('stamp/' . $school_id);
         }
-
-
         $timestamp->update([
             'end' => Carbon::now()->toTimeString(),
         ]);
--- resources/views/user/stamp.blade.php
+++ resources/views/user/stamp.blade.php
@@ -18,40 +18,64 @@
 @endsection
 
 @section('userarea')
-@if ($personal)
+@if (isset($personal))
 <p class="username">
     <span class="display-4">
 
         {{ $personal->getName() }}</span>さん
 </p>
 <div class="stamp d-flex justify-content-around">
-<form action="start/{{ $school->id }}" method="POST">
+    <form action="start/{{ $school->id }}" method="POST">
 
-    {{ csrf_field() }}
-    <input type="hidden" name="user_id" value="{{ $personal->id }}">
-    <input type="submit" value=" IN " class="button">
+        {{ csrf_field() }}
+        <input type="hidden" name="user_id" value="{{ $personal->id }}">
+        <input type="submit" value=" IN " class="button">
 
-</form>
-<form action="end/{{ $school->id }}" method="POST">
+    </form>
+    <form action="end/{{ $school->id }}" method="POST">
 
-    {{ csrf_field() }}
-    <input type="hidden" name="user_id" value="{{ $personal->id }}">
-    <input type="submit" value="OUT" class="button">
+        {{ csrf_field() }}
+        <input type="hidden" name="user_id" value="{{ $personal->id }}">
+        <input type="submit" value="OUT" class="button">
 
-</form>
+    </form>
 </div>
 
 @else
 <p class="username">
-    右リストから利用者名を選択して下さい
+
+    {{ $alerttxt }}
 </p>
 @endif
 @endsection
 
 @section('userslist')
 @foreach ($users as $user)
-<a href="{{ $school->id }}?id={{ $user->id }}" class="list-group-item list-group-item-action">
-    {{$user->id . '：' . $user->getName() }}</a>
+<a href="{{ $school->id }}?id={{ $user->id }}" class="d-flex justify-content-between list-group-item list-group-item-action">
+    <dl class="namebox d-inline-block">
+        <dt class="namebox_id d-inline-block text-right">
+
+            {{ $user->id }}</dt>：
+
+        <dd class="namebox_name d-inline-block">
+
+            {{ $user->getName() }}</dd>
+    </dl>
+
+    @if ( array_key_exists ($user->id , $attendlist ) )
+
+    @if ( $attendlist[$user->id] == true )
+    <p class="namebox_attend_in d-inline-block text-center">
+
+        出席中</p>
+    @else
+    <p class="namebox_attend_out d-inline-block text-center">
+
+        退席済</p>
+    @endif
+    @endif
+
+</a>
 @endforeach
 @endsection
 
